ae825014c198afbb0494c470e30907e45ff07020
Foo0s <oraproavp@list.ru>
2023-09-09 00:48:33 +0300

    Done las-pz

--- PZ/PZ_3/fiveteen.java
+++ PZ/PZ_3/fiveteen.java
@@ -5,9 +5,45 @@ public class fiveteen {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int x = number / 10; int v = number / 5; int l = number / 50; int c = number / 100;
-        if (number / 10 > 0 && number < 50){
-            System.out.println("X".repeat(x) + "V".repeat(number - v));
+        String answer = "";
+        if (number >= 1 && number <= 100){
+            if (number >= 90){
+                answer += "XC";
+                number -= 90;
+            }
+            if (number > 60){
+                answer += "LX";
+                number -= 60;
+            }
+            if (number >= 40){
+                answer += "XL";
+                number -= 40;
+            }
+            if (number >= 50){
+                answer += "L";
+                number -= 50;
+            }
+            if (number < 40){
+                answer += "X".repeat(number/10);
+                number -= 10*(number/10);
+            }
+            if (number == 9){
+                answer += "IX".repeat(number/9);
+                number -= 9;
+            }
+            if (number >= 5){
+                answer += "V".repeat(number/5);
+                number -= 5*(number/5);
+            }
+            if (number == 4){
+                answer += "IV";
+                number -= 4;
+            }
+            else{
+                answer += "I".repeat(number);
+                number -= number;
+            }
         }
+        System.out.println(answer);
     }
 }
