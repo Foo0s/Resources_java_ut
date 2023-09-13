60b737c5ea27ac56ea9a66136a77f68cfa719577
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-09-13 17:39:02 +0300

    PZ-3

---
+++ PZ/PZ_6/PZ6_1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Введите число n: ");
+      int x = sc.nextInt();
+      System.out.println("Введите число m: ");
+      int y = sc.nextInt(); int chet = 1;
+      
+      int[][] massive = new int[x][y];
+      System.out.println("Построение массива: ");
+      for (int i = 0; i < x; i++){
+          for (int k = 0; k < y; k++){
+            massive[i][k] = (k+1)*chet;
+          }
+          chet += 1;
+      }
+      for (int i = 0; i < x; i++){
+          for (int k = 0; k < y; k++){
+            System.out.print(massive[i][k] + " ");   
+          }
+          System.out.println("\n");
+      }
+    }
+}
---
+++ PZ/PZ_6/PZ6_2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Введите число n: ");
+      int x = sc.nextInt();
+      System.out.println("Введите число m: ");
+      int y = sc.nextInt();
+      int[][] mas = new int[x][y];
+      int d = 0; int s = 1;
+      
+      for (int i = 0; i < x; i++){
+          for (int k = 0; k < y; k++){
+              mas[i][k] = 0;
+          }
+          mas[i][d] = 1;
+          for (int k = 0; k < s-1; k++){
+              mas[i][k] = 2;
+          }
+          d++;
+          s++;
+      }
+      for (int i = 0; i < x; i++){
+          for (int k = 0; k < y; k++){
+              System.out.print(mas[i][k] + " ");
+          }
+          System.out.println("\n");
+      }
+    }
+}
