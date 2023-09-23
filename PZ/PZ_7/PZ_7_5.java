4b30afa95e54e7f0fbb36a8dcc7efa1cc50874a8
Foo0s <oraproavp@list.ru>
2023-09-23 12:22:08 +0300

    Done PZ-7

--- PZ/PZ_7/PZ_7_5.java
+++ PZ/PZ_7/PZ_7_5.java
@@ -1,31 +1,50 @@
 package PZ.PZ_7;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.Character;
+
+import static java.lang.Character.*;
+
 public class PZ_7_5 {
     public static void main(String[] args){
         Scanner ns = new Scanner(System.in);
         System.out.println("Количество строк в книге: ");
         int k_book = ns.nextInt();
-        String[] book_r = new String[k_book+1];
-        for(int i=0; i<k_book+1; i++){
+        ns.nextLine();
+        String[] book_r = new String[k_book];
+        for(int i=0; i<k_book; i++){
             System.out.println("Введите строку книги: ");
-            String s_name = ns.nextLine();
-            book_r[i] = s_name;
+            String text = ns.nextLine();
+            book_r[i] = text;
         }
         System.out.println("Введите количество продуктов: ");
         int m_produkt = ns.nextInt();
-        String[] arr_allerg = new String[m_produkt];
-        for(int i = 0; i < m_produkt+1; i++){
-            if (i > m_produkt){
-                break;
-            }
-            else{
-                System.out.println("Введите продукт: ");
-                arr_allerg[i] = Arrays.toString(ns.nextLine().split("-"));
+        ns.nextLine();
+        for(int i = 0; i < m_produkt; i++){
+            System.out.println("Введите продукт: ");
+            String[] array_word = ns.nextLine().split(" - ");
+            for(int j = 0; j < book_r.length; j++){
+                String[] new_arr = book_r[j].split(" ");
+                for(int s = 0; s < new_arr.length; s++){
+                    if(new_arr[s].equalsIgnoreCase(array_word[0])){
+                        if(isUpperCase(new_arr[s].charAt(0))){
+                            String result = array_word[1].replace(array_word[1].charAt(0), toUpperCase(array_word[1].charAt(0)));
+                            new_arr[s] = result;
+                        }
+                        else{
+                            new_arr[s] = array_word[1];
+                        }
+                    };
+                };
+                String sd = "";
+                for(String word: new_arr){
+                    sd += word + " ";
+                }
+                book_r[j] = sd;
             }
         }
-        for(int i = 0; i < m_produkt; i++){
-            System.out.println(arr_allerg[i]);
+        for(int i = 0; i < book_r.length; i++){
+            System.out.println(book_r[i]);
         }
     }
 }
