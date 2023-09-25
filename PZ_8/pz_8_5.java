439bcc2b4e8cb60eece98cd28c720fee26bc9c27
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-09-25 14:08:53 +0300

    Create pz_8_5.java
    done 5

---
+++ PZ_8/pz_8_5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Введите количество запросов: ");
+		int kol = sc.nextInt(); sc.nextLine();
+		HashMap<String, String> hash_l = new HashMap<>();
+		for(int i = 0; i < kol; i++){
+		    System.out.println("Ваш запрос (push/pop): ");
+		    String od = sc.nextLine();
+		    if(od.equals("push")){
+		        System.out.println("Введите название одежды: ");
+		        String s_text = sc.nextLine();
+		        hash_l.put("Петя", s_text);
+		    }
+		    else if (od.equals("pop")){
+		        System.out.println("Введите название одежды: ");
+		        String s_text = sc.nextLine();
+		        hash_l.remove("Петя", s_text);
+		        System.out.println("Петя забрал " + s_text);
+		    }
+		}
+		System.out.println("Одежда оставшаяся на стуле: " + hash_l.get("Петя"));
+	}
+}
