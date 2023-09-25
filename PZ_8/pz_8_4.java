50e0c8a4f15f778395637b193c4af26f40bad292
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-09-25 14:08:29 +0300

    Create pz_8_4.java
    done 4

---
+++ PZ_8/pz_8_4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Введите размер списка: ");
+		int size_l = sc.nextInt();
+		ArrayList<String> list = new ArrayList<>();
+		for(int i = 0; i < size_l; i++){
+		    String s = "" + i;
+		    list.add(s);
+		}
+		System.out.println("Введите индекс для удаления: ");
+		int del_index = sc.nextInt();
+		list.remove(del_index);
+		System.out.println("Список: " + list);
+	}
+}
