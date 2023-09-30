da2dc189a32d9c0e30a10e8f123f420fdf833477
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-09-30 13:17:13 +0300

    Create PZ_9_3.java

---
+++ PZ/PZ_9/PZ_9_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+
+//Вывести имена сотрудников какой профессии больше всего, вывести кол-воБ профессию и их имена в том же порядке в котором вводились.
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int numb = sc.nextInt();
+        HashSet<Integer> h_list = new HashSet();
+        for(int j = 0; j < numb; j++){
+            System.out.println("Введите число: ");
+            int numb_t = sc.nextInt();
+            if (h_list.contains(numb_t) == true){
+                System.out.println("YES");
+            }
+            else System.out.println("NO"); h_list.add(numb_t);
+        }
+    }
+}
