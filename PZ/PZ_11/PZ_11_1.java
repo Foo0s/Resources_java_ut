501d18269597d34e16df78637b5bfddc2c8735c1
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-05 12:11:46 +0300

    Create PZ_11_1.java
    work1

---
+++ PZ/PZ_11/PZ_11_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Факториал числа: ");
+        int f_number = sc.nextInt();
+        System.out.println("Факториал числа = " + fact(f_number));
+    }
+
+    static int fact(int f_numb){
+        if (f_numb == 0) return 1;
+        if(f_numb < 0) {
+            System.out.println("Число должно быть неотрицательным!");
+            return 0;
+        }
+        return fact(f_numb-1)*f_numb;
+    }
+}
