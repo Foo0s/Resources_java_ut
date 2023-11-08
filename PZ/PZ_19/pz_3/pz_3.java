bf634b20fae30ebca08841e096ade3885a9bc602
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-11-08 18:11:23 +0300

    Create pz_3.java

---
+++ PZ/PZ_19/pz_3/pz_3.java
@@ -0,0 +1,10 @@
+package PZ.PZ_19.pr_3;
+
+public class pr_3 {
+    public static void main(String[] args) {
+        /*
+            Создайте небольшое окно и панель ContentPane с двумя кнопками с использование библиотеки swing.
+        */
+        new Content_Frame();
+    }
+}
