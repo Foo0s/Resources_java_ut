af0c2a12966678fd35819795fb926892a729165d
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-11-08 18:10:17 +0300

    Create pr_2.java

---
+++ PZ/PZ_19/pr_2/pr_2.java
@@ -0,0 +1,13 @@
+package PZ.PZ_19.pr_2;
+import javax.swing.*;
+import java.awt.*;
+
+import static javax.swing.JLayeredPane.*;
+
+public class pr_2 extends JFrame{
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new Object_JLayeredPane();
+    }
+}
